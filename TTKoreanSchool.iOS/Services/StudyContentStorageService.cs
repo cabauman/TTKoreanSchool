4d0d87dcaf8619356b7a05f7c58b979fd04ac0f5
Colt Bauman <cabauman@maverickgames.co>
2018-03-24 18:59:26 +0900

    Remove most of the Firebase storage code
    - Most of this code was either for the Xamarin bindings package or testing. I'll redo this code from scratch when I move on to implementing FirebaseStoarge.

--- TTKoreanSchool.iOS/Services/StudyContentStorageService.cs
+++ TTKoreanSchool.iOS/Services/StudyContentStorageService.cs
@@ -1,26 +1,25 @@
-﻿//using System;
-//using System.IO;
-//using TTKoreanSchool.Services;
-//using TTKoreanSchool.Services.Interfaces;
+﻿using System;
+using System.IO;
+using TTKoreanSchool.Services;
+using TTKoreanSchool.Services.Interfaces;
 
-//namespace TTKoreanSchool.iOS.Services
-//{
-//    public class StudyContentStorageService : BaseStudyContentStorageService
-//    {
-//        public StudyContentStorageService(string authToken = null, IStudyContentDataService dataService = null)
-//            : base(dataService)
-//        {
-//        }
+namespace TTKoreanSchool.iOS.Services
+{
+    public class StudyContentStorageService : BaseStudyContentStorageService
+    {
+        public StudyContentStorageService()
+        {
+        }
 
-//        protected override string LocalRootDirectory
-//        {
-//            get
-//            {
-//                string documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-//                string root = Path.Combine(documents, "..", "Library");
+        protected override string LocalRootDirectory
+        {
+            get
+            {
+                string documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string root = Path.Combine(documents, "..", "Library");
 
-//                return root;
-//            }
-//        }
-//    }
-//}
+                return root;
+            }
+        }
+    }
+}
--- TTKoreanSchool/Services/Interfaces/IStudyContentStorageService.cs
+++ TTKoreanSchool/Services/Interfaces/IStudyContentStorageService.cs
@@ -1,13 +1,6 @@
-﻿using System.Threading.Tasks;
-
-namespace TTKoreanSchool.Services.Interfaces
+﻿namespace TTKoreanSchool.Services.Interfaces
 {
     public interface IStudyContentStorageService
     {
-        Task<string> GetVocabImageUrl(string filename);
-
-        Task<string> GetVocabAudioLocalPath(string filename);
-
-        Task<string> GetSentenceAudioLocalPath(string filename);
     }
 }
