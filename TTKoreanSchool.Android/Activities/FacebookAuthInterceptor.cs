2088810eae469bd2fb9c08520580e400c9e90f28
Colt Bauman <cabauman@maverickgames.co>
2018-04-02 14:46:42 +0900

    Add interceptors for Facebook & Google auth
    - Required by Android to redirect from the "native sign in UI" (CustomTabs) back to the app.

---
+++ TTKoreanSchool.Android/Activities/FacebookAuthInterceptor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using TTKoreanSchool.Config;
+
+namespace TTKoreanSchool.Android.Activities
+{
+    [Activity(Label = "FacebookAuthInterceptor")]
+    [
+        IntentFilter
+        (
+            actions: new[] { Intent.ActionView },
+            Categories = new[]
+            {
+                    Intent.CategoryDefault,
+                    Intent.CategoryBrowsable
+            },
+            DataSchemes = new[]
+            {
+                FacebookAuthConfig.DATA_SCHEME,
+            },
+            DataHosts = new[]
+            {
+                "authorize",
+            }
+            ,
+            DataPaths = new[]
+            {
+                "/",
+            }
+        )
+    ]
+    public class FacebookAuthInterceptor : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            global::Android.Net.Uri uri_android = Intent.Data;
+
+            #if DEBUG
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine("FacebookAuthInterceptor.OnCreate()");
+            sb.Append("     uri_android = ").AppendLine(uri_android.ToString());
+            System.Diagnostics.Debug.WriteLine(sb.ToString());
+            #endif
+
+            var uri = new Uri(uri_android.ToString());
+
+            // load redirect_url Page
+            //SignInActivity.FacebookAuth?.OnPageLoading(uri_netfx);
+
+            Finish();
+        }
+    }
+}
---
+++ TTKoreanSchool.Android/Activities/GoogleAuthInterceptor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using TTKoreanSchool.Config;
+
+namespace TTKoreanSchool.Android.Activities
+{
+    [Activity(Label = "GoogleAuthInterceptor")]
+    [
+        IntentFilter
+        (
+            actions: new[] { Intent.ActionView },
+            Categories = new[]
+            {
+                    Intent.CategoryDefault,
+                    Intent.CategoryBrowsable
+            },
+            DataSchemes = new[]
+            {
+                // First part of the redirect url
+                GoogleAuthConfig.DATA_SCHEME_ANDROID,
+            },
+            DataPaths = new[]
+            {
+                "/oauth2redirect"
+            }
+        )
+    ]
+    public class GoogleAuthInterceptor : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            global::Android.Net.Uri uri_android = Intent.Data;
+
+            #if DEBUG
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine("GoogleAuthInterceptor.OnCreate()");
+            sb.Append("     uri_android = ").AppendLine(uri_android.ToString());
+            System.Diagnostics.Debug.WriteLine(sb.ToString());
+            #endif
+
+            var uri = new Uri(uri_android.ToString());
+
+            // Send the URI to the Authenticator for continuation
+            //SignInActivity.GoogleAuth?.OnPageLoading(uri);
+
+            Finish();
+        }
+    }
+}
