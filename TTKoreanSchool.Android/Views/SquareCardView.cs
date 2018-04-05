18fb56ff93fd7ddada54e15e17331f1fbd69890c
Colt Bauman <cabauman@maverickgames.co>
2018-04-05 18:07:21 +0900

    Add custom square view for grid recycler view

---
+++ TTKoreanSchool.Android/Views/SquareCardView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace TTKoreanSchool.Android.Views
+{
+    [Register("ttkoreanschool/android/views/SquareCardView")]
+    public class SquareCardView : CardView
+    {
+        public SquareCardView(Context context, IAttributeSet attrs)
+            : base(context, attrs)
+        {
+        }
+
+        public SquareCardView(Context context, IAttributeSet attrs, int defStyle)
+            : base(context, attrs, defStyle)
+        {
+        }
+
+        protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+        {
+            base.OnMeasure(widthMeasureSpec, widthMeasureSpec);
+        }
+    }
+}
