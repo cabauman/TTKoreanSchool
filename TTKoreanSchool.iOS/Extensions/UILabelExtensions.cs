68ea108cfba9893b0e7c2dd58bcda3cccc14ac4c
Colt Bauman <cabauman@maverickgames.co>
2018-04-09 17:49:59 +0900

    Add extensions for UIView & UILabel

---
+++ TTKoreanSchool.iOS/Extensions/UILabelExtensions.cs
@@ -0,0 +1,52 @@
+﻿using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace TTKoreanSchool.iOS.Extensions
+{
+    public static class UILabelExtensions
+    {
+        public static void AdjustFontSizeToHeight(this UILabel label)
+        {
+            var words = label.Text.Split(' ');
+            string longestWord = words[0];
+            for(int i = 1; i < words.Length; ++i)
+            {
+                if(words[i].Length > longestWord.Length)
+                {
+                    longestWord = words[i];
+                }
+            }
+
+            NSString nsString = new NSString(longestWord);
+            const int minFontSize = 10;
+            int size = 32;
+            for(; size >= minFontSize; --size)
+            {
+                var font = label.Font.WithSize(size);
+
+                UIStringAttributes attribs = new UIStringAttributes { Font = font };
+                CGSize frameSize = nsString.GetSizeUsingAttributes(attribs);
+                if(frameSize.Width < label.Bounds.Width)
+                {
+                    label.Font = font;
+                    break;
+                }
+            }
+
+            CGSize sizeToDisplay = new CGSize(label.Bounds.Width, float.MaxValue);
+            nsString = new NSString(label.Text);
+            for(; size >= 10; --size)
+            {
+                var font = label.Font.WithSize(size);
+                CGSize frameSize = UIStringDrawing.StringSize(nsString, font, sizeToDisplay, UILineBreakMode.TailTruncation);
+
+                if(frameSize.Width <= label.Bounds.Width && frameSize.Height <= label.Bounds.Height)
+                {
+                    label.Font = font;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ TTKoreanSchool.iOS/Extensions/UIViewExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+using UIKit;
+
+namespace TTKoreanSchool.iOS.Extensions
+{
+    public static class UIViewExtensions
+    {
+        public static void FlipHorizontaly(this UIView view, bool isIn, double duration = 0.3, Action onFinished = null)
+        {
+            var m34 = (nfloat)(-1 * 0.0003);
+
+            var minAlpha = (nfloat)0.0f;
+            var maxAlpha = (nfloat)1.0f;
+
+            view.Alpha = (nfloat)1.0;
+
+            var minTransform = CATransform3D.Identity;
+            minTransform.m34 = m34;
+            minTransform = minTransform.Rotate((nfloat)((isIn ? 1 : -1) * Math.PI * 0.5), 0.0f, 1.0f, 0.0f);
+            var maxTransform = CATransform3D.Identity;
+            maxTransform.m34 = m34;
+
+            view.Alpha = isIn ? minAlpha : maxAlpha;
+            view.Layer.Transform = isIn ? minTransform : maxTransform;
+            UIView.Animate(
+                duration,
+                0,
+                UIViewAnimationOptions.CurveEaseInOut,
+                () =>
+                {
+                    view.Layer.AnchorPoint = new CGPoint(0.5f, 0.5f);
+                    view.Layer.Transform = isIn ? maxTransform : minTransform;
+                    view.Alpha = isIn ? maxAlpha : minAlpha;
+                },
+                onFinished
+            );
+        }
+    }
+}
