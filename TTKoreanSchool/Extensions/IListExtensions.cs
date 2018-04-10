6a25522c15d905cea4a0ab24298960c58cfc3ee1
Colt Bauman <cabauman@maverickgames.co>
2018-04-10 22:02:13 +0900

    Add utility class to handle random numbers

--- TTKoreanSchool/Extensions/IListExtensions.cs
+++ TTKoreanSchool/Extensions/IListExtensions.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using TTKoreanSchool.Utils;
 
 namespace TTKoreanSchool.Extensions
 {
     public static class IListExtensions
     {
-        public static Random Rand { get; } = new Random();
-
         // Fisher–Yates shuffle
         public static void Shuffle<T>(this IList<T> list)
         {
             for(int i = 0; i < list.Count; ++i)
             {
                 T temp = list[i];
-                int randIdx = Rand.Next(i, list.Count);
+                int randIdx = RandomUtil.Rand.Next(i, list.Count);
                 list[i] = list[randIdx];
                 list[randIdx] = temp;
             }
---
+++ TTKoreanSchool/Utils/RandomUtil.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TTKoreanSchool.Utils
+{
+    public static class RandomUtil
+    {
+        public static Random Rand { get; } = new Random();
+    }
+}
