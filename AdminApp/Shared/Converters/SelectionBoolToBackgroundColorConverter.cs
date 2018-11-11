0201707998dcab64d154d0d2a8130267b08d8a9b
cabauman <cab452005@hotmail.com>
2018-11-11 21:42:17 +0900

    Add converter for chaning selected item bg color

---
+++ AdminApp/Shared/Converters/SelectionBoolToBackgroundColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TongTongAdmin.Converters
+{
+    public class SelectionBoolToBackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value == true ? Color.FromRgb(211, 211, 211) : Color.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
