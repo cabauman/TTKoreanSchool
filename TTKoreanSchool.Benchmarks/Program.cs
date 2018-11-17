ac2e1ccecedf6b2ffe34c397361751165fde8481
cabauman <cab452005@hotmail.com>
2018-11-17 10:03:05 +0900

    Add benchmark project

---
+++ TTKoreanSchool.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace TTKoreanSchool.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
