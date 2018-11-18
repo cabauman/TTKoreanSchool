44015e8826f254dfa74d9f3ee0abb6af72153f2e
cabauman <cab452005@hotmail.com>
2018-11-18 16:11:00 +0900

    Add audiobook properties for image and audio names

--- TTKSCore/Models/Audiobook.cs
+++ TTKSCore/Models/Audiobook.cs
@@ -7,8 +7,14 @@ namespace TTKSCore.Models
         [JsonProperty("title")]
         public string Title { get; set; }
 
+        [JsonProperty("imageName")]
+        public string ImageName { get; set; }
+
         [JsonProperty("imageUrl")]
         public string ImageUrl { get; set; }
+
+        [JsonProperty("audioName")]
+        public string AudioName { get; set; }
 
         [JsonProperty("audioUrl")]
         public string AudioUrl { get; set; }
