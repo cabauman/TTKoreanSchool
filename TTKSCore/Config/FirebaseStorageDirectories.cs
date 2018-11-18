eedc0975308714afbe9c769d6baea6ebb2faf12c
cabauman <cab452005@hotmail.com>
2018-11-18 16:12:16 +0900

    Add file for Firebase storage directory constants

---
+++ TTKSCore/Config/FirebaseStorageDirectories.cs
@@ -0,0 +1,8 @@
+﻿namespace TTKSCore.Config
+{
+    public static class FirebaseStorageDirectories
+    {
+        public static string AUDIOBOOK_IMAGES = "audiobook-images";
+        public static string AUDIOBOOK_AUDIO = "audiobook-audio";
+    }
+}
