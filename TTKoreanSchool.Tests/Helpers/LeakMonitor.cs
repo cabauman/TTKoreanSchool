609979e4e528f5a5d8952318110d45f89a6b092e
cabauman <cab452005@hotmail.com>
2018-11-19 00:16:12 +0900

    Add LeakMonitor and BuilderExtensions

---
+++ TTKoreanSchool.Tests/Helpers/LeakMonitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TTKoreanSchool.Tests
+{
+    public class LeakMonitor<T>
+        where T : class
+    {
+        private readonly WeakReference _reference;
+
+        public LeakMonitor(T itemToWatch)
+        {
+            _reference = new WeakReference(itemToWatch);
+        }
+
+        public T Item => _reference.Target as T;
+
+        public bool IsItemAlive()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            return _reference.IsAlive;
+        }
+    }
+}
