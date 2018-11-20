767d7b7f1b7a8c76f3cc3d0cb176b0652626ab4a
cabauman <cab452005@hotmail.com>
2018-11-20 14:07:39 +0900

    Add path builder methods for Firebase Storage

--- TTKSCore/Config/FirebaseStorageDirectories.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TTKSCore.Config
-{
-    public static class FirebaseStorageDirectories
-    {
-        public static string AUDIOBOOK_IMAGES = "audiobook-images";
-        public static string AUDIOBOOK_AUDIO = "audiobook-audio";
-    }
-}
---
+++ TTKSCore/Config/FirebaseStorageHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace TTKSCore.Config
+{
+    public static class FirebaseStorageHelper
+    {
+        public static string AUDIOBOOK_IMAGES = "audiobook-images";
+        public static string AUDIOBOOK_AUDIO = "audiobook-audio";
+
+        public static string AudiobookImagePath(string filename)
+        {
+            return $"{AUDIOBOOK_IMAGES}/{filename}";
+        }
+
+        public static string AudiobookAudioPath(string filename)
+        {
+            return $"{AUDIOBOOK_AUDIO}/{filename}";
+        }
+    }
+}
