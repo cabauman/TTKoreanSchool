2da6d8934d3994a91b23a149a1a31fa6d8455e4c
cabauman <cab452005@hotmail.com>
2018-11-25 16:42:03 +0900

    Add StudyContentConfig

---
+++ TTKSCore/Common/StudyContentConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TTKSCore.Common
+{
+    public static class StudyContentConfig
+    {
+        public static readonly List<string> WordClasses = new List<string>
+        {
+            "Verb",
+            "Noun",
+            "Adjective",
+            "Adverb",
+            "Pronoun",
+            "Numeral",
+            "Interjection",
+            "Particle",
+            "Determiner",
+            "Hangul",
+            "Phrase",
+            "Grammar",
+        };
+
+        public static readonly List<string> Transitivity = new List<string>
+        {
+            "Transitive",
+            "Intransitive",
+            "Both",
+        };
+    }
+}
