6dc1d1b9dac3eeae8a8dd8f7e139edb7d3327d96
cabauman <cab452005@hotmail.com>
2018-11-25 16:41:50 +0900

    [Admin] Add StudyContentService
    - Keeps a cache of the various dependent lists

---
+++ AdminApp/Shared/Services/StudyContentService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using DynamicData;
+using TTKSCore.Models;
+
+namespace TongTongAdmin.Services
+{
+    public class StudyContentService
+    {
+        public StudyContentService()
+        {
+            VocabCache = new SourceCache<VocabTerm, string>(x => x.Id);
+            GrammarCache = new SourceCache<GrammarPrinciple, string>(x => x.Id);
+            SentenceCache = new SourceCache<ExampleSentence, string>(x => x.Id);
+            VocabImageCache = new SourceCache<VocabImage, string>(x => x.Id);
+        }
+
+        public ISourceCache<VocabTerm, string> VocabCache { get; }
+
+        public ISourceCache<GrammarPrinciple, string> GrammarCache { get; }
+
+        public ISourceCache<ExampleSentence, string> SentenceCache { get; }
+
+        public ISourceCache<VocabImage, string> VocabImageCache { get; }
+    }
+}
