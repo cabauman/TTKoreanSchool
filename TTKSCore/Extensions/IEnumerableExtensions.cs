28c0c48dc2f1485dc5864f4e81f1118f2a4852a3
cabauman <cab452005@hotmail.com>
2018-12-08 16:11:31 +0900

    Add Do extension method for IEnumerable

---
+++ TTKSCore/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TTKSCore
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Do<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source)
+            {
+                action(element);
+            }
+
+            return source;
+        }
+    }
+}
