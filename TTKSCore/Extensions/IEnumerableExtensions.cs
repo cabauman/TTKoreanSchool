f3b801280afbd5b09bd5296c4b56885172846f50
cabauman <cab452005@hotmail.com>
2018-12-27 18:31:40 +0900

    Add partition extension method

--- TTKSCore/Extensions/IEnumerableExtensions.cs
+++ TTKSCore/Extensions/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TTKSCore
 {
@@ -13,6 +14,39 @@ namespace TTKSCore
             }
 
             return source;
+        }
+
+        /// <summary>
+        /// Splits the sequence into two sequences, containing the elements 
+        /// for which the given predicate returns <c>true</c> and <c>false</c> 
+        /// respectively.
+        /// </summary>
+        public static void Partition<T>(
+            this IEnumerable<T> source,
+            Func<T, bool> predicate,
+            out IEnumerable<T> trueSequence,
+            out IEnumerable<T> falseSequence)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+
+            IEnumerable<T> fs = null;
+            IEnumerable<T> ts = null;
+
+            foreach (var g in source.GroupBy(predicate))
+            {
+                if (g.Key)
+                {
+                    ts = g;
+                }
+                else
+                {
+                    fs = g;
+                }
+            }
+
+            trueSequence = ts ?? Enumerable.Empty<T>();
+            falseSequence = fs ?? Enumerable.Empty<T>();
         }
     }
 }
